e05459b8b3a79ca5ce9eb78bb92cbdecc4f539e8
pratikit007 <62968775+pratikit007@users.noreply.github.com>
2021-01-14 21:21:08 -0500

    Update clock.jsx

--- src/component/clock.jsx
+++ src/component/clock.jsx
@@ -1,1 +1,35 @@
 
+import React, { Component } from 'react';
+
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }    
+    tick = () => {
+        if(this.props.timerFlag){
+        this.setState({
+            date: new Date()
+          });
+        }
+      }  
+      
+      componentDidMount() {
+        this.timerID = setInterval(
+        () => this.tick(),
+        1000
+        );
+      }
+    
+    render(){
+     
+        return( 
+            <>
+            {this.props.timerFlag && <p>Current Clock time: {this.state.date.toLocaleTimeString()}</p>}
+            </>
+        )
+    }
+}
+
+
+export default Clock;
